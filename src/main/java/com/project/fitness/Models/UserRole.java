6332a0cf7c537e3a301bc69ed30408dd8be9ef18
Lokesh Jahure <imail4lokesh@gmail.com>
2026-06-27 14:05:24 +0530

    Initial commit

---
+++ src/main/java/com/project/fitness/Models/UserRole.java
@@ -0,0 +1,5 @@
+package com.project.fitness.Models;
+
+public enum UserRole {
+    USER, ADMIN
+}
---
+++ src/main/java/com/project/fitness/security/CustomUserDetailsService.java
@@ -0,0 +1,34 @@
+package com.project.fitness.security;
+
+import com.project.fitness.Models.User;
+import com.project.fitness.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.security.autoconfigure.SecurityProperties;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+       User user = userRepository.findByEmail(email);
+       if (user == null) {
+           throw new RuntimeException("User not found" + email);
+       }
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getEmail())
+                .password(user.getPassword())
+                .roles(user.getRole().name())
+                .build();
+    }
+}
